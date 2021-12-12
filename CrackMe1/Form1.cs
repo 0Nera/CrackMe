481e521a7306c648b99fcca6e63b3545e743f52c
Aren Elchinyn <slizen.can@gmail.com>
2021-12-12 15:47:29 +0300

    Init

---
+++ CrackMe1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CrackMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if(textBox1.Text == "4203") {
+                MessageBox.Show("Great. Отлично!");
+            } else {
+                MessageBox.Show("Password incorect. Неверный пароль!");
+            }
+        }
+    }
+}
